bd4954f0a17b4e5a34476999e8227c33a24f9f09
= <=>
2024-02-16 00:12:14 +0600

    Create Stripe user when user signin

--- app/api/auth/auth-option.ts
+++ app/api/auth/auth-option.ts
@@ -2,6 +2,7 @@ import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import Stripe from "stripe";
 
 const authOption: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -13,6 +14,29 @@ const authOption: AuthOptions = {
   ],
   session: {
     strategy: "jwt",
+  },
+  events: {
+    createUser: async ({ user }) => {
+      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+      // Create a stripe customer
+      if (user.name && user.email) {
+        const customer = await stripe.customers.create({
+          name: user.name,
+          email: user.email,
+        });
+
+        // Update user with the stripe customerId
+        await prisma.user.update({
+          where: {
+            id: user.id,
+          },
+          data: {
+            stripeCustomerId: user.id,
+          },
+        });
+      }
+    },
   },
 };
 
