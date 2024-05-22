d271b968f27d79035146659d6c391313b2c8d8aa
= <=>
2024-05-22 22:54:32 +0600

    Fix build errors

--- app/api/auth/auth-option.ts
+++ app/api/auth/auth-option.ts
@@ -2,7 +2,6 @@ import prisma from "@/prisma/client";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import Stripe from "stripe";
 
 const authOption: AuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -14,29 +13,6 @@ const authOption: AuthOptions = {
   ],
   session: {
     strategy: "jwt",
-  },
-  events: {
-    createUser: async ({ user }) => {
-      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
-
-      // Create a stripe customer
-      if (user.name && user.email) {
-        const customer = await stripe.customers.create({
-          name: user.name,
-          email: user.email,
-        });
-
-        // Update user with the stripe customerId
-        await prisma.user.update({
-          where: {
-            id: user.id,
-          },
-          data: {
-            stripeCustomerId: user.id,
-          },
-        });
-      }
-    },
   },
 };
 
